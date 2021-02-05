a667df6383d83c20f76453ca6a291037040a5bf0
ronny-souza <ronnymarinho2@gmail.com>
2021-02-05 20:06:02 -0300

    Função que calcula a hipotenusa

---
+++ Exercicio01.py
@@ -0,0 +1,17 @@
+# QUESTÃO 01 - Escreva uma função que receba o tamanho de dois catetos de um triângulo como seus parâmetros e 
+# calcule a hipotenusa. A função deve apenas imprimir o resultado.
+#
+# h² = ca² + co²
+
+import math
+
+def calculaHipotenusa(ca, co):
+    resultado = ((ca**2) + (co**2))
+    hipotenusa = math.sqrt(resultado)
+    return hipotenusa
+
+
+ca = int(input("Digite o valor do primeiro cateto: "))
+co = int(input("Digite o valor do segundo cateto: "))
+
+print(f"Hipotenusa = {calculaHipotenusa(ca, co)}")
